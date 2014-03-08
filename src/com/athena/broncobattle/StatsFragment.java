200f75ad801f07a5ab5dd0bd86fbeede80a452cd
Nic Corna <nscornia@gmail.com>
2014-03-08 12:48:55 -0700

    stas view

--- src/com/athena/broncobattle/StatsFragment.java
+++ src/com/athena/broncobattle/StatsFragment.java
@@ -1,25 +1,29 @@
 package com.athena.broncobattle;
 
-import android.app.Fragment;
+import java.util.Arrays;
+
+import android.app.ListFragment;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-public class StatsFragment extends Fragment {
+public class StatsFragment extends ListFragment {
 
 	int nextLevelThreshold = 0;
 	int totalExperiencePoints = 184;
 	int experiencePoints = 0;
 	int level = 0;
-	
+
 	private CountDownTimer timer;
-	
+
 	int timerTime = 500;
-	int TIMER_REFRESH = 60; //60 Hz
+	int TIMER_REFRESH = 60; // 60 Hz
 
 	private final int experienceFuntionX = 100;
 	private final double experienceFunctionY = 1.2;
@@ -28,8 +32,8 @@ public class StatsFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, final ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.stats_fragment_layout, container, false);
 
-		timer =  new CountDownTimer(timerTime, timerTime/TIMER_REFRESH) {
-			
+		timer = new CountDownTimer(timerTime, timerTime / TIMER_REFRESH) {
+
 			public void onTick(long millisUntilFinished) {
 				updateBar(millisUntilFinished);
 			}
@@ -39,19 +43,36 @@ public class StatsFragment extends Fragment {
 				close();
 			}
 		};
-		
+
 		return view;
 	}
-	
+
 	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		level = calculateLevel();
-		
+
 		TextView levelText = (TextView) view.findViewById(R.id.level_title);
 		levelText.setText("Level " + level);
+		
+		Achievement[] achievements = new Achievement[] { 
+				new Achievement("id1", "", "foo", "blah di blah"),
+				new Achievement("id1", "", "foo", "blah di blah"),
+				new Achievement("id1", "", "foo", "blah di blah"),
+				new Achievement("id1", "", "foo", "blah di blah"),
+				new Achievement("id1", "", "foo", "blah di blah"),
+				new Achievement("id1", "", "foo", "blah di blah"),
+				new Achievement("id1", "", "foo", "blah di blah"),
+				new Achievement("id1", "", "foo", "blah di blah"),
+				new Achievement("id1", "", "foo", "blah di blah"),
+				new Achievement("id1", "", "foo", "blah di blah")
+
+		};
+
+		ArrayAdapter<Achievement> adapter = new StatsListAdapter(getActivity().getApplicationContext(), 0, 0, Arrays.asList(achievements));
+
+		((ListView) getView().findViewById(R.id.stats_list)).setAdapter(adapter);
 	};
-	
-	
+
 	@Override
 	public void onViewStateRestored(Bundle savedInstanceState) {
 		super.onViewStateRestored(savedInstanceState);
@@ -64,7 +85,7 @@ public class StatsFragment extends Fragment {
 
 		for (int loopLevel = 1;; loopLevel++) {
 			int previousLevelThreshold = (int) (experienceFuntionX * Math.pow(loopLevel - 1, experienceFunctionY));
-			nextLevelThreshold = (int) (experienceFuntionX * Math.pow(loopLevel, experienceFunctionY)); 
+			nextLevelThreshold = (int) (experienceFuntionX * Math.pow(loopLevel, experienceFunctionY));
 			sumThreshold += nextLevelThreshold;
 			if (sumThreshold > totalExperiencePoints) {
 				rval = loopLevel - 1;
@@ -82,19 +103,20 @@ public class StatsFragment extends Fragment {
 		timer.cancel();
 		super.onPause();
 	}
-	
+
 	public void updateBar(double progress) {
 		ProgressBar p = (ProgressBar) getView().findViewById(R.id.experience_bar);
-		if(progress==0){
-			p.setProgress((int) (100 * (experiencePoints/ (double) nextLevelThreshold)));
-		}else{
-			p.setProgress((int) ((100 * (experiencePoints/ (double) nextLevelThreshold)) * (1 - (progress/timerTime))));
+		if (progress == 0) {
+			p.setProgress((int) (100 * (experiencePoints / (double) nextLevelThreshold)));
+		} else {
+			p.setProgress((int) ((100 * (experiencePoints / (double) nextLevelThreshold)) * (1 - (progress / timerTime))));
 		}
 	}
 
 	public void close() {
 		timer.cancel();
-		//ProgressBar p = (ProgressBar) getView().findViewById(R.id.experience_bar);
-		//p.setProgress(experiencePoints / nextLevelThreshold);
+		// ProgressBar p = (ProgressBar)
+		// getView().findViewById(R.id.experience_bar);
+		// p.setProgress(experiencePoints / nextLevelThreshold);
 	}
 }
